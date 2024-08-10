1abcfdf48c2f74ca95b7fdb622f8b451f1482d75
Guess Who <ARGimadeyev@stud.kpfu.ru>
2024-08-10 18:13:36 +0300

    testik 2

--- WiFi_Thief/WiFi.ps1
+++ WiFi_Thief/WiFi.ps1
@@ -1,6 +1,3 @@
-$Signature = ″[DllImport(«user32.dll»)]public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow); ″
-$ShowWindowAsync = Add-Type -MemberDefinition $Signature -Name «Win32ShowWindowAsync» -Namespace Win32Functions -PassThru
-$ShowWindowAsync::ShowWindowAsync((Get-Process -Id $pid).MainWindowHandle, 0)
 
 if ('ru-RU' -eq $PSCulture)
 {
